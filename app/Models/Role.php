d42ba08527a62792f2bea6327be9bde9379d9ba8
Yohannes Mekonnen <johannesm1219@gmail.com>
2022-12-05 23:05:28 +0300

    define model relationship

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
-    use HasFactory;
+    use HasUuids, HasFactory, SoftDeletes;
+
+    protected $table = 'roles';
+
+    protected $fillable = ['name'];
+
+    protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Model
+class User extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, HasUuids, SoftDeletes, HasApiTokens, Notifiable;
+
+    protected $table = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $guarded = [];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = ['updated_at', 'deleted_at'];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
